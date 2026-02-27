032a74f499b6d689113a4a5719cfafdedeef4a1f
Alba <castillamackin@gmail.com>
2026-02-27 16:15:21 +0100

    CPU, memory usage and top processes selections added

--- systMonitor.sh
+++ systMonitor.sh
@@ -3,6 +3,26 @@
 LOG_FILE="syst_monitor_log.txt"
 ARCHIVE_DIR="ArchiveLogs"
 
+
+
+see_usage() {
+    echo "CPU USAGE"
+    top -bn1 | grep "Cpu"
+    echo "MEMORY USAGE"
+    free -h
+    log_action "CPU and memory usage displayed"
+}
+
+see_top_processes() {
+    echo "TOP 10 MEMORY PROCESSES"
+    ps -eo pid,user,%cpu,%mem,comm --sort=-%mem | head -n 11
+    log_action "top 10 memory consuming processes displayed"
+}
+
+
+
+
+
 log_action() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> $LOG_FILE
 }
@@ -17,8 +37,8 @@ while true; do
     read -p "Choose an option: " choice
 
     case $choice in
-        1) ;;
-        2) ;;
+        1) see_usage ;;
+        2) see_top_processes;;
         3) ;;
         4) ;;
         5) ;;
